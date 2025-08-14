ec47819795cd6c50ddb562e1ac5565a2e1169f2e
Shakir <shakirsayyed7378@gmail.com>
2025-08-14 17:13:36 +0530

    End of section 11

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -6,6 +6,7 @@ import { tap } from 'rxjs';
 
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -34,6 +35,20 @@ export class MemberService {
 
   updateMember(member: EditableMember) {
     return this.http.put(this.baseUrl + 'members', member);
+  }
+
+  uploadPhoto(file:File) {
+    const formData = new FormData();
+    formData.append('file',file);
+    return this.http.post<Photo>(this.baseUrl + 'members/add-photo', formData);
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'members/set-main-photo/' + photo.id, {});
+  }
+
+  deletePhoto(photoId: number) {
+   return this.http.delete(this.baseUrl + 'members/delete-photo/' + photoId);
   }
   
 }
