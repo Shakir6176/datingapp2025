f37853b05005ee9723f7db7f53c5cf899bc78eab
Shakir <shakirsayyed7378@gmail.com>
2025-08-18 16:49:00 +0530

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,9 +10,14 @@ public class RegisterDto
 
     [Required]
     [EmailAddress]
-    public string Email { get; set; }= "";
+    public string Email { get; set; } = "";
 
     [Required]
     [MinLength(6)]
     public string Password { get; set; } = "";
+
+    [Required] public string Gender { get; set; } = string.Empty;
+    [Required] public string City { get; set; } = string.Empty;
+    [Required] public string Country { get; set; } = string.Empty;
+    [Required] public DateOnly DateOfBirth { get; set; }
 }
