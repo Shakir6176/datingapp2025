f12acdeb4a4b830629b01236e2fc2714af386084
Shakir <shakirsayyed7378@gmail.com>
2025-08-22 14:16:02 +0530

    End of section 14

--- client/src/features/members/member-card/member-card.ts
+++ client/src/features/members/member-card/member-card.ts
@@ -1,8 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../../types/member';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { LikesService } from '../../../core/services/likes-service';
 @Component({
   selector: 'app-member-card',
   imports: [RouterLink,AgePipe],
@@ -10,6 +11,20 @@ import { AgePipe } from '../../../core/pipes/age-pipe';
   styleUrl: './member-card.css'
 })
 export class MemberCard {
-
+  private likeService = inject(LikesService);
   member = input.required<Member>();
+  protected hasLiked = computed(() => this.likeService.likeIds().includes(this.member().id));
+
+  toggleLike(event: Event) {
+    event.stopPropagation();
+    this.likeService.toggleLike(this.member().id).subscribe({
+      next: () =>{
+        if (this.hasLiked()) {
+          this.likeService.likeIds.update(ids => ids.filter(x => x !== this.member().id))
+        } else {
+          this.likeService.likeIds.update(ids => [...ids, this.member().id])
+        }
+      }
+    })
+  }
 }
